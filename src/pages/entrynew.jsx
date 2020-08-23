00e8630b622a74bc81a80ca852618ebf2a0ac141
JacFox11 <mauricioaguilera494@gmail.com>
2020-08-23 16:39:33 -0600

    pagina para gregar apunte

--- src/pages/entrynew.jsx
+++ src/pages/entrynew.jsx
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Jumbotron, Container, Button, InputGroup, FormControl } from 'react-bootstrap';
+import './Home.css';
 
-export default class Home extends Component {
+export default class entrynew extends Component {
     render() {
         return (
-            <div>
-                <h2>Hello</h2>
-            </div>
+        <Container fluid>
+            <Jumbotron>
+                <h2>Nuevo Apunte</h2>
+                <InputGroup className="mb-3">
+                    <InputGroup.Prepend>
+                        <InputGroup.Text id="basic-addon1">Descripcion</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                        aria-label="Description"
+                        aria-describedby="basic-addon1"
+                    />
+                </InputGroup>
+                <InputGroup className="mb-3">
+                    <InputGroup.Prepend>
+                        <InputGroup.Text id="basic-addon1">Etiquetas</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                        placeholder="Ej. React, Bootstrap, etc."
+                        aria-label="Tags"
+                        aria-describedby="basic-addon1"
+                    />
+                </InputGroup>
+            </Jumbotron>
+            <Link to="/">
+                <Button BsStyle="Primary">Guardar</Button>
+            </Link>
+        </Container>
         )
     }
 }
